255936406a36eefa92a0c842ee45fc81018f71ac
ceasarj27 <cesarjimenez@ou.edu>
2020-11-02 16:56:41 -0600

    More code

--- src/AlwaysContinues.java
+++ src/AlwaysContinues.java
@@ -5,6 +5,12 @@
  */
 public class AlwaysContinues {
 
+	public T peek() {
+		if(isEmpty())
+			return null;
+		else 
+			return items[top];
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
